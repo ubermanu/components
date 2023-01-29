424eda18356e747af9b37a5e5e6e1876155706aa
ubermanu <manu@paca.happy-dev.fr>
2023-01-29 13:34:44 +0100

    first commit

---
+++ src/day-cell.js
@@ -0,0 +1,168 @@
+import { css, html, LitElement } from "lit";
+
+/**
+ * Implements an accessible day cell.
+ * Renders its day with aria-* attributes.
+ */
+export class DayCell extends LitElement {
+  static get properties() {
+    return {
+      /**
+       * The date of the day cell in the ISO format (e.g. 2023-01-29T11:53:27.319Z).
+       */
+      date: { type: String, reflect: true },
+
+      /**
+       * If disabled, the day cell will not be focusable.
+       */
+      disabled: { type: Boolean, reflect: true },
+
+      /**
+       * If selected, the day cell will be marked as selected.
+       * The day cell will not be focusable.
+       * The day cell will not be selectable.
+       */
+      selected: { type: Boolean, reflect: true },
+    };
+  }
+
+  constructor() {
+    super();
+    this.date = "";
+  }
+
+  /**
+   * Get the day of the day cell.
+   * @returns {number}
+   */
+  get day() {
+    return new Date(this.date).getDate();
+  }
+
+  /**
+   * Get the month of the day cell.
+   * @returns {number}
+   */
+  get month() {
+    return new Date(this.date).getMonth();
+  }
+
+  /**
+   * Get the year of the day cell.
+   * @returns {number}
+   */
+  get year() {
+    return new Date(this.date).getFullYear();
+  }
+
+  /**
+   * Get the language of the day cell.
+   * @returns {string}
+   */
+  get lang() {
+    return (
+      this.getAttribute("lang") ||
+      this.grid?.lang ||
+      document.documentElement.lang ||
+      "en"
+    );
+  }
+
+  /**
+   * Returns true if the day cell is today.
+   * @returns {boolean}
+   */
+  get isToday() {
+    const today = new Date();
+    const date = new Date(this.date);
+    return (
+      today.getDate() === date.getDate() &&
+      today.getMonth() === date.getMonth() &&
+      today.getFullYear() === date.getFullYear()
+    );
+  }
+
+  /**
+   * Returns the day grid that contains this day cell.
+   * @returns {Element}
+   */
+  get grid() {
+    return this.closest("day-grid");
+  }
+
+  /**
+   * Returns true if the day cell is the first day of the month.
+   * @returns {boolean}
+   */
+  get isFirstDayOfMonth() {
+    return this.day === 1;
+  }
+
+  /**
+   * Returns true if the day cell is the last day of the month.
+   * @returns {boolean}
+   */
+  get isLastDayOfMonth() {
+    const date = new Date(this.date);
+    const nextDate = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+    return this.day === nextDate.getDate() - 1;
+  }
+
+  /**
+   * Returns true if the day cell is the first day of the year.
+   * @returns {boolean}
+   */
+  get isFirstDayOfYear() {
+    return this.day === 1 && this.month === 0;
+  }
+
+  /**
+   * Returns true if the day cell is the last day of the year.
+   * @returns {boolean}
+   */
+  get isLastDayOfYear() {
+    return this.day === 31 && this.month === 11;
+  }
+
+  /**
+   * Returns true if the day cell is the first day of the week.
+   * @returns {boolean}
+   */
+  get isFirstDayOfWeek() {
+    const date = new Date(this.date);
+    return date.getDay() === 0;
+  }
+
+  /**
+   * Returns true if the day cell is the last day of the week.
+   * @returns {boolean}
+   */
+  get isLastDayOfWeek() {
+    const date = new Date(this.date);
+    return date.getDay() === 6;
+  }
+
+  get formattedDate() {
+    const date = new Date(this.date);
+    return date.toLocaleDateString(this.lang, {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  }
+
+  render() {
+    return html`<span aria-label="${this.formattedDate}">${this.day}</span>`;
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+    `;
+  }
+}
+
+window.customElements.define("day-cell", DayCell);
---
+++ src/day-grid.js
@@ -0,0 +1,198 @@
+import { css, html, LitElement } from "lit";
+import "./day-cell.js";
+
+/**
+ * Implements an accessible day grid.
+ * Exposes some methods to control its view (e.g. previousMonth, nextMonth, etc.)
+ */
+export class DayGrid extends LitElement {
+  static get properties() {
+    return {
+      /**
+       * The current month being displayed.
+       */
+      month: { type: Number, reflect: true },
+
+      /**
+       * The current year being displayed.
+       */
+      year: { type: Number, reflect: true },
+    };
+  }
+
+  constructor() {
+    super();
+
+    // The current month and year are set to the current date.
+    const today = new Date();
+    this.month = today.getMonth();
+    this.year = today.getFullYear();
+  }
+
+  /**
+   * Get the language of the grid.
+   * @returns {string}
+   */
+  get lang() {
+    return this.getAttribute("lang") || document.documentElement.lang || "en";
+  }
+
+  /**
+   * Moves the calendar to the previous month.
+   * If the current month is January, it will move to December of the previous year.
+   * If the current year is not set, it will not move.
+   * If the current month is not set, it will not move.
+   */
+  previousMonth() {
+    if (this.year === -1 || this.month === -1) {
+      return;
+    }
+
+    if (this.month === 0) {
+      this.month = 11;
+      this.year--;
+    } else {
+      this.month--;
+    }
+
+    this.requestUpdate();
+  }
+
+  /**
+   * Moves the calendar to the next month.
+   * If the current month is December, it will move to January of the next year.
+   * If the current year is not set, it will not move.
+   * If the current month is not set, it will not move.
+   */
+  nextMonth() {
+    if (this.year === -1 || this.month === -1) {
+      return;
+    }
+
+    if (this.month === 11) {
+      this.month = 0;
+      this.year++;
+    } else {
+      this.month++;
+    }
+
+    this.requestUpdate();
+  }
+
+  /**
+   * Moves the calendar to the previous year.
+   * If the current year is not set, it will not move.
+   */
+  prevYear() {
+    if (this.year === -1) {
+      return;
+    }
+
+    this.year--;
+    this.requestUpdate();
+  }
+
+  /**
+   * Moves the calendar to the next year.
+   * If the current year is not set, it will not move.
+   */
+  nextYear() {
+    if (this.year === -1) {
+      return;
+    }
+
+    this.year++;
+    this.requestUpdate();
+  }
+
+  /**
+   * Returns an array of days for the current month.
+   * The array will contain 42 days (6 weeks).
+   * The days of the previous month will be added to the beginning of the array.
+   * The days of the next month will be added to the end of the array.
+   * @returns {Array}
+   */
+  get days() {
+    const days = [];
+
+    if (this.year === -1 || this.month === -1) {
+      return days;
+    }
+
+    const firstDay = new Date(this.year, this.month, 1);
+    const lastDay = new Date(this.year, this.month + 1, 0);
+
+    // Add the days of the previous month.
+    for (let i = firstDay.getDay() - 1; i >= 0; i--) {
+      const date = new Date(this.year, this.month, 1 - i);
+      days.push({
+        date: date,
+        disabled: true,
+      });
+    }
+
+    // Add the days of the current month.
+    for (let i = 1; i <= lastDay.getDate(); i++) {
+      const date = new Date(this.year, this.month, i);
+      days.push({
+        date: date,
+        disabled: false,
+      });
+    }
+
+    // Add the days of the next month.
+    for (let i = 1; days.length < 42; i++) {
+      const date = new Date(this.year, this.month + 1, i);
+      days.push({
+        date: date,
+        disabled: true,
+      });
+    }
+
+    return days;
+  }
+
+  get selectedDays() {
+    return this.days.filter((day) => day.selected);
+  }
+
+  select(day) {
+    day.selected = true;
+    this.requestUpdate();
+  }
+
+  deselect(day) {
+    day.selected = false;
+    this.requestUpdate();
+  }
+
+  createRenderRoot() {
+    return this;
+  }
+
+  render() {
+    return html`
+      ${this.days.map(
+        (day) => html`<day-cell
+          .date=${day.date.toISOString()}
+          .disabled=${day.disabled}
+          .selected=${day.selected}
+        ></day-cell>`
+      )}
+    `;
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: grid;
+        grid-template-columns: repeat(7, 1fr);
+        grid-template-rows: repeat(6, 1fr);
+        grid-gap: 0.5rem;
+        padding: 0.5rem;
+      }
+    `;
+  }
+}
+
+window.customElements.define("day-grid", DayGrid);
